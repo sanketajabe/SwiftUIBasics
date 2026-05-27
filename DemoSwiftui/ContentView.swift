572d75622c5083cb191bb519cac15d4a7ba1dbda
Sanket Ajabe <sanket@muvi.com>
2026-05-27 22:13:45 +0530

    Initial Commit

---
+++ DemoSwiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoSwiftui
+//
+//  Created by Muvi on 27/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoSwiftui/DemoSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftuiApp.swift
+//  DemoSwiftui
+//
+//  Created by Muvi on 27/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
